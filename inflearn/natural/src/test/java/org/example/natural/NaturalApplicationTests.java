82415238239eb42669c680b6da7a436445cca168
JungMin-E <dlwjdals273@naver.com>
2025-02-10 20:56:54 +0900

    section 1

---
+++ inflearn/natural/src/test/java/org/example/natural/NaturalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.natural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
